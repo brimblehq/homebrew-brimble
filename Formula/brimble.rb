d503c0baa9854efeba4ecca8070e5d0c2267be38
Muritala David <davmuri1414@gmail.com>
2022-07-24 22:08:26 +0100

    liscense

--- Formula/brimble.rb
+++ Formula/brimble.rb
@@ -3,6 +3,7 @@ class Brimble < Formula
     homepage "https://github.com/brimblehq/homebrew-brimble"
     url "https://github.com/brimblehq/homebrew-brimble/releases/download/v1.0.0/brimble-macos-x64.tar.gz"
     sha256 "d2534745bf7f8e45ad3a17b79b690e644ba0083413adefd6222aa9bcde54e5c4"
+    license "MIT"
     version "1.0.0"
     def install
       bin.install "brimble"
