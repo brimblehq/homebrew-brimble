2b87ca011fb885a0a3831fc68ec8cd01b9b119ac
Muritala David <davmuri1414@gmail.com>
2022-07-24 23:05:19 +0100

    update

--- Formula/brimble.rb
+++ Formula/brimble.rb
@@ -1,10 +1,10 @@
 class Brimble < Formula
     desc "Brimble command line interface that provides a simple, intuitive interface for interacting with Brimble making it easy to deploy, test, and manage JAMSTACK deployments without having to leave your terminal."
     homepage "https://github.com/brimblehq/homebrew-brimble"
-    url "https://github.com/brimblehq/homebrew-brimble/releases/download/v1.0.0/brimble-macos-x64.tar.gz"
-    sha256 "d2534745bf7f8e45ad3a17b79b690e644ba0083413adefd6222aa9bcde54e5c4"
+    url "https://github.com/brimblehq/homebrew-brimble/releases/download/v1.0.1/brimble.tar.gz"
+    sha256 "ff54c27a49729a76ec00e2bd8377c275bd4ca8c2eee7cf9f30f221a32a71206c"
     license "MIT"
-    version "1.0.0"
+    version "1.0.1"
     def install
       bin.install "brimble"
     end
