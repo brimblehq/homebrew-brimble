25b26be16ef31b02c2772684e3f4512504e4ad6a
Muritala David <davmuri1414@gmail.com>
2022-07-24 21:57:00 +0100

    feat(formula): add formula brimble

---
+++ Formula/brimble.rb
@@ -0,0 +1,10 @@
+class Brimble < Formula
+    desc "Brimble command line interface that provides a simple, intuitive interface for interacting with Brimble making it easy to deploy, test, and manage JAMSTACK deployments without having to leave your terminal."
+    homepage "https://github.com/brimblehq/homebrew-brimble"
+    url "https://github.com/brimblehq/homebrew-brimble/releases/download/v1.0.0/brimble-macos-x64.tar.gz"
+    sha256 "d2534745bf7f8e45ad3a17b79b690e644ba0083413adefd6222aa9bcde54e5c4"
+    version "1.0.0"
+    def install
+      bin.install "brimble"
+    end
+  end
